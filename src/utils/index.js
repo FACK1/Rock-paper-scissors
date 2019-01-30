220792d1533486d43fc7ef95fbbcc1ee3feb86ef
Amin <ayman201266@hotmail.com>
2019-01-30 11:36:37 +0200

    create computer component #12

---
+++ src/utils/index.js
@@ -0,0 +1,17 @@
+export function getRandomChoice() {
+  const choices = ['rock', 'paper', 'scissors'];
+
+  return choices[Math.floor(Math.random() * choices.length)];
+}
+
+export function getWinner(choice, computerChoice) {
+
+  if (choice === computerChoice) return 'draws';
+
+  else if (
+      (computerChoice === 'scissors' && choice === 'rock')  || (computerChoice === 'paper' && choice === 'scissors') || (computerChoice === 'rock' && choice === 'paper')
+    )
+  return 'wins';
+
+  else return 'losses';
+}
